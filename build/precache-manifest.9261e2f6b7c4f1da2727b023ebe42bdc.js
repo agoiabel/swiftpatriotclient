45eff7000a2e109eac11efeeed30f32feec5c9bb
Abel Agoi Adeyemi <agoiabeladeyemi@gmail.com>
2019-01-21 15:49:44 +0100

    update

---
+++ build/precache-manifest.9261e2f6b7c4f1da2727b023ebe42bdc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc3059e69259b0cf5a413e0a704265d7",
+    "url": "/static/media/logo@2x.cc3059e6.png"
+  },
+  {
+    "revision": "e7c0cc5ea00ed0bdc0b769ace32e4835",
+    "url": "/static/media/bg-4.e7c0cc5e.jpg"
+  },
+  {
+    "revision": "a055d6e6b89a4834348b16cac7b91df3",
+    "url": "/static/media/bg-3.a055d6e6.jpg"
+  },
+  {
+    "revision": "367d0668c6984f7fd8017995a7926149",
+    "url": "/static/media/bg-2.367d0668.jpg"
+  },
+  {
+    "revision": "084551f94307aea833e843c80a9fb43f",
+    "url": "/static/media/bg-1.084551f9.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e1038bb6115ba8716cd3",
+    "url": "/static/js/main.e1038bb6.chunk.js"
+  },
+  {
+    "revision": "3a441b0ea6edbfc13929",
+    "url": "/static/js/1.3a441b0e.chunk.js"
+  },
+  {
+    "revision": "e1038bb6115ba8716cd3",
+    "url": "/static/css/main.56c5f8f3.chunk.css"
+  },
+  {
+    "revision": "d64992cbfa38f8327aff0c8e5ce755f7",
+    "url": "/index.html"
+  }
+];
